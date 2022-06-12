c7d01bfdf28d5ad737e6a278a08a6fb0592e52ab
Mohammad Iqbal <mohammad.iqbal.2409@gmail.com>
2022-06-12 22:43:14 +0700

    Membuat fungsi promiseOutput

--- promise.js
+++ promise.js
@@ -1,7 +1,16 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emotion) => {
+  try {
+    let filmTheaterIXX = await promiseTheaterIXX();
+    let filmTheater = filmTheaterIXX.concat(await promiseTheaterVGC());
+
+    return filmTheater;
+  } catch(err) {
+    return err;
+  }
+};
 
 module.exports = {
   promiseOutput,
