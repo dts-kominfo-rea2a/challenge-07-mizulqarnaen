cba5d6b4e9d79608bed121098fba1bf306e286f7
Mohammad Iqbal <mohammad.iqbal.2409@gmail.com>
2022-06-12 22:48:12 +0700

    Menyesuaikan fungsi promiseOutput

--- promise.js
+++ promise.js
@@ -3,12 +3,12 @@ const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
 const promiseOutput = async (emotion) => {
   try {
-    let filmTheaterIXX = await promiseTheaterIXX();
-    let filmTheater = filmTheaterIXX.concat(await promiseTheaterVGC());
+    let filmTheater = (await promiseTheaterIXX()).concat(await promiseTheaterVGC());
 
     return filmTheater.filter(data => {
       return data.hasil == emotion;
     }).length;
+    
   } catch(err) {
     return err;
   }
